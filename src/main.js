1340df484741c4bbb451d632f74091eaeffb6602
robingrit <robin.nilsson@gritacademy.se>
2022-04-19 11:43:45 +0200

    fixed type bug

--- src/main.js
+++ src/main.js
@@ -7,14 +7,15 @@ document.getElementById("happinesBtn").addEventListener("click", happines);
 document.getElementById("hungerBtn").addEventListener("click", hunger);
 const inputField = document.querySelector("input");
 const select = document.querySelector("select");
+let tama;
 
 function start() {
   // ändring tar input data när man klickar på knappen
-  const tama = new Tamagochi(inputField.value, select.value);
+  tama =  new Tamagochi(inputField.value, select.value);
   img.src = tama.whattype();
   tama.start();
 
-  let creature = "  creature: " + select.value;
+  let creature = "<br>  creature: " + select.value;
   let name = "Name: " + inputField.value + creature;
 
   document.querySelector("h2").innerHTML = name;
